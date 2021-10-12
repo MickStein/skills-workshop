5db504934a7b5aaa5d51d610766cb6f9ceb9be40
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-12 11:19:55 +0100

    Added a Rspec test for find_book method

--- test_driven_development/writing_tests/spec/library_spec.rb
+++ test_driven_development/writing_tests/spec/library_spec.rb
@@ -1,5 +1,11 @@
+require "library"
 describe Library do
   it 'can find a specific book' do
+    library_items = Library.new
+
+    library_items.find_book('POODR')
+
+    expect(library_items.find_book('POODR')).to eq({title: 'POODR', author: 'Sandi Metz', subject: 'OOP'})
   end
 
   it 'can add a new book' do
