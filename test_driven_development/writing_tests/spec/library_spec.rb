37728508d17ae8ee8487d5668bb1669b07b35a9d
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-14 09:28:21 +0100

    Practicing with tests, created find and add book tests

--- test_driven_development/writing_tests/spec/library_spec.rb
+++ test_driven_development/writing_tests/spec/library_spec.rb
@@ -1,4 +1,5 @@
 require "library"
+
 describe Library do
   it 'can find a specific book' do
     library_items = Library.new
@@ -9,6 +10,17 @@ describe Library do
   end
 
   it 'can add a new book' do
+    library_items = Library.new
+
+    library_items.add_book({title: 'Fire Storm', author: 'Bill Tablot', subject: 'Fantasy'})
+
+    expect(library_items.add_book({title: 'Fire Storm', author: 'Bill Tablot', subject: 'Fantasy'}).to eq([
+      {title: 'POODR', author: 'Sandi Metz', subject: 'OOP'},
+      {title: 'Learn Ruby The Hard Way', author: 'Zed Shaw', subject: 'Ruby'},
+      {title: 'Eloquent JavaScript', author: 'Marijn Haverbeke', subject: 'JS'},
+      {title: 'The Well Grounded Rubyist', author: 'Sandi Metz', subject: 'Ruby'},
+      {title: 'Fire Storm', author: 'Bill Tablot', subject: 'Fantasy'}
+    ])
   end
 
   it 'can remove a book' do
